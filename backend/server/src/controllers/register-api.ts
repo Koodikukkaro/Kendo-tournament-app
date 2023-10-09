7a94084c44a796285ac2a071aab35e1069f5bddb
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-10-09 19:31:26 +0300

    updated register-api based on requirement. user schema updated. js-doc updated

--- backend/server/src/controllers/register-api.ts
+++ backend/server/src/controllers/register-api.ts
@@ -3,11 +3,10 @@ import User from '../models/user-model.js';
 
 /**
  * @swagger
- * /api/users/register:
+ * /api/register:
  *   post:
  *     summary: Register a new user
- *     tags:
- *       - User
+ *     tags: [User]
  *     requestBody:
  *       required: true
  *       content:
@@ -17,15 +16,39 @@ import User from '../models/user-model.js';
  *             required:
  *               - email
  *               - password
+ *               - firstName
+ *               - lastName
+ *               - phoneNumber
+ *               - clubName
+ *               - danRank
  *             properties:
  *               email:
  *                 type: string
  *                 example: user@example.com
- *                 description: Valid email address
  *               password:
  *                 type: string
  *                 example: mypassword123
- *                 description: Alphanumeric and 8-30 characters long
+ *               firstName:
+ *                 type: string
+ *                 example: John
+ *               lastName:
+ *                 type: string
+ *                 example: Doe
+ *               phoneNumber:
+ *                 type: string
+ *                 example: +1234567890
+ *               clubName:
+ *                 type: string
+ *                 example: EliteClub
+ *               danRank:
+ *                 type: string
+ *                 example: 1st Dan
+ *               underage:
+ *                 type: boolean
+ *                 example: false
+ *               guardiansEmail:
+ *                 type: string
+ *                 example: guardian@example.com
  *     responses:
  *       200:
  *         description: Successfully registered user
@@ -36,10 +59,8 @@ import User from '../models/user-model.js';
  *               properties:
  *                 state:
  *                   type: boolean
- *                   example: true
  *                 message:
  *                   type: string
- *                   example: "user registration successful"
  *       400:
  *         description: Registration failed
  *         content:
@@ -50,8 +71,9 @@ import User from '../models/user-model.js';
  *                 error:
  *                   type: string
  * */
+
 export const registerAPI = async (req: any, res: any) => {
-    const { email, password } = req.body;
+    const { email, password, firstName, lastName, phoneNumber, clubName, danRank, underage, guardiansEmail } = req.body;
 
     // Email validation using a simple regular expression
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -79,13 +101,19 @@ export const registerAPI = async (req: any, res: any) => {
     const newUser = new User({
         email,
         password: hashedPassword,
+        firstName,
+        lastName,
+        phoneNumber,
+        clubName,
+        danRank,
+        underage: underage || false,
+        guardiansEmail,
     });
 
     try {
         const savedUser = await newUser.save();
         res.json({
-            "state": true,
-            "message": "user registration successful"
+            "message": "User registration successful"
         });
     } catch (err) {
         res.status(400).json({ error: err });
--- backend/server/src/models/user-model.ts
+++ backend/server/src/models/user-model.ts
@@ -3,6 +3,13 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    clubName: { type: String, required: true },
+    danRank: { type: String, required: true },
+    underage: { type: Boolean, default: false }, // Default to false if not provided
+    guardiansEmail: { type: String }, // Optional, so not required
 });
 
 const User = mongoose.model('User', userSchema);
