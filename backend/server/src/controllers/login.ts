f6f84f41d871af3813a948985fedfa35c0b39b4e
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-10-09 19:21:26 +0300

    implemented login api

---
+++ backend/server/src/controllers/login.ts
@@ -0,0 +1,66 @@
+import bcrypt from 'bcryptjs';
+import User from '../models/user-model.js';
+
+/**
+ * @swagger
+ * /api/login:
+ *   post:
+ *     summary: Login existing user
+ *     tags:
+ *       - User
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: user@example.com
+ *               password:
+ *                 type: string
+ *                 example: mypassword123
+ *     responses:
+ *       200:
+ *         description: Successfully logged in
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user_id:
+ *                   type: string
+ *       401:
+ *         description: Unauthorized
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ * */
+
+export const loginAPI = async (req: any, res: any) => {
+    const { email, password } = req.body;
+
+    // Check if the email exists
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    // Check if the password matches
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+        return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    //TODO: generate session, refreshtoken or whatever else needed
+    // Return the user_id
+    res.json({ user_id: user._id });
+};
