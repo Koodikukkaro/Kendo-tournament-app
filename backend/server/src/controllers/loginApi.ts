609f67d9f27244907ea6b6c485829ebd13eb7073
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-10-10 14:55:07 +0300

    changed swagger documentation to json based

--- backend/server/src/controllers/loginApi.ts
+++ backend/server/src/controllers/loginApi.ts
@@ -1,73 +1,7 @@
+import { Express } from 'express';
 import bcrypt from 'bcryptjs';
 import User from '../models/user-model.js';
-import { Express } from 'express';
-/**
- * @swagger
- * /api/user/login:
- *   post:
- *     summary: Login existing user
- *     tags:
- *       - User
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - email
- *               - password
- *             properties:
- *               email:
- *                 type: string
- *                 example: user@example.com
- *               password:
- *                 type: string
- *                 example: mypassword123
- *     responses:
- *       200:
- *         description: Successfully logged in
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 user_id:
- *                   type: string
- *                 first_name:
- *                   type: string
- *                 last_name:
- *                   type: string
- *                 email:
- *                   type: string
- *                 phone:
- *                   type: string
- *                 club:
- *                   type: string
- *                 dan:
- *                   type: string
- *                 underage:
- *                   type: boolean
- *                 guardian:
- *                   type: string
- *                 createdOn:
- *                   type: string
- *                   format: date-time
- *                   example: '2021-10-04T08:30:00.000Z'
- *                 lastUpdatedOn:
- *                   type: string
- *                   format: date-time
- *                   example: '2021-10-04T09:30:00.000Z'
- *       401:
- *         description: Unauthorized
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- * */
+
 
 export const loginAPI = async (req: Express.Request, res: Express.Response) => {
     const { email, password } = req.body;
