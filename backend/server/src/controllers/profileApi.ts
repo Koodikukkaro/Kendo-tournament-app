236c6735552473b8c709aa74be7fda59112589ea
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-10-10 05:28:22 +0300

    implemented get profile API

---
+++ backend/server/src/controllers/profileApi.ts
@@ -0,0 +1,86 @@
+import User from '../models/user-model.js';
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   get:
+ *     summary: Retrieve user profile information.
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: User ID
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved user profile
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user_id:
+ *                   type: string
+ *                 first_name:
+ *                   type: string
+ *                 last_name:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *                 phone:
+ *                   type: string
+ *                 club:
+ *                   type: string
+ *                 dan:
+ *                   type: string
+ *                 underage:
+ *                   type: boolean
+ *                 guardian:
+ *                   type: string
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *       500:
+ *         description: An error occurred while retrieving the user profile
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ */
+
+export const getProfileAPI = async (req: any, res: any) => {
+    const userId = req.params.id;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        res.json({
+            user_id: user._id,
+            first_name: user.firstName,
+            last_name: user.lastName,
+            email: user.email,
+            phone: user.phoneNumber,
+            club: user.clubName,
+            dan: user.danRank,
+            underage: user.underage,
+            guardian: user.guardiansEmail
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: "An error occurred while retrieving the user profile" });
+    }
+};
--- backend/server/src/routes/userRoutes.ts
+++ backend/server/src/routes/userRoutes.ts
@@ -4,9 +4,11 @@ const userRoutes = express.Router();
 // Import controller methods
 import { registerAPI } from '../controllers/registerApi.js';
 import { loginAPI } from '../controllers/loginApi.js';
+import { getProfileAPI } from '../controllers/profileApi.js';
 
 // Define routes
 userRoutes.post('/register', registerAPI);
 userRoutes.post('/login', loginAPI);
+userRoutes.get('/:id', getProfileAPI);
 
 export default userRoutes;
