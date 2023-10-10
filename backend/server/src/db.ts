76b5e9d762f109a3ea91f69d660956a8e65abbee
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-10-10 16:24:17 +0300

    lint applied to fix errors on db.ts file

--- backend/server/src/db.ts
+++ backend/server/src/db.ts
@@ -1,20 +1,26 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
-const mongoUri: string = process.env.MONGO_URL!;
+const mongoUri = process.env.MONGO_URL;
+if (mongoUri === null || mongoUri === undefined) {
+  throw new Error("MONGO_URL is not defined in environment variables.");
+}
 
 console.log("_---------------_");
 console.log(mongoUri);
 console.log("_---------------_");
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
-        console.log('MongoDB connection successful');
-    } catch (err) {
-        console.error(`MongoDB connection error: ${err}`);
-    }
+const connectDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB connection successful");
+  } catch (err) {
+    console.error(`MongoDB connection error: ${String(err)}`);
+  }
 };
 
 export default connectDB;
