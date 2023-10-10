f6da3b118f95bd5707625e006fd2a0e1c49bac7b
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-10-10 16:30:33 +0300

    lint applied & fixed errors in all the remaining files

--- backend/server/src/routes/index.ts
+++ backend/server/src/routes/index.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import userRoutes from './userRoutes.js';
+import express from "express";
+import userRoutes from "./userRoutes.js";
 
 const mainRouter = express.Router();
 
 // Use the imported routes
-mainRouter.use('/user', userRoutes);
+mainRouter.use("/user", userRoutes);
 
 export default mainRouter;
--- backend/server/src/routes/userRoutes.ts
+++ backend/server/src/routes/userRoutes.ts
@@ -1,14 +1,22 @@
-import express from 'express';
-const userRoutes = express.Router();
+import express from "express";
 
 // Import controller methods
-import { registerAPI } from '../controllers/registerApi.js';
-import { loginAPI } from '../controllers/loginApi.js';
-import { getProfileAPI } from '../controllers/profileApi.js';
+import { registerAPI } from "../controllers/registerApi.js";
+import { loginAPI } from "../controllers/loginApi.js";
+import { getProfileAPI } from "../controllers/profileApi.js";
+const userRoutes = express.Router();
 
 // Define routes
-userRoutes.post('/register', registerAPI);
-userRoutes.post('/login', loginAPI);
-userRoutes.get('/:id', getProfileAPI);
+userRoutes.post("/register", (req, res, next) => {
+  registerAPI(req, res).catch(next);
+});
+
+userRoutes.post("/login", (req, res, next) => {
+  loginAPI(req, res).catch(next);
+});
+
+userRoutes.get("/:id", (req, res, next) => {
+  getProfileAPI(req, res).catch(next);
+});
 
 export default userRoutes;
