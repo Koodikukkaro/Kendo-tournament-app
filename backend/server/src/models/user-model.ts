a448ab98594a2741e5149d4848ea1cba8318d0f3
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-10-19 20:33:06 +0300

    fixed the bug in user-model

--- backend/server/src/models/user-model.ts
+++ backend/server/src/models/user-model.ts
@@ -1,19 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 
-
-interface User {
-  email: string,
-  password: string,
-  phoneNumber: string,
-  firstName: string,
-  lastName: string,
-  clubName: string,
-  danRank: string,
-  underage?: boolean,
-  guardiansEmail?: string
+interface UserInterface {
+  email: string;
+  password: string;
+  phoneNumber: string;
+  firstName: string;
+  lastName: string;
+  clubName: string;
+  danRank: string;
+  underage?: boolean;
+  guardiansEmail?: string;
 }
 
-const userSchema = new Schema(
+const userSchema = new Schema<UserInterface>(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
