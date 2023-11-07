2cad10300192de54428da42ebf48ff2f09583b4c
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-11-07 16:11:10 +0200

    added landing page, connected landing page with login and registration

--- frontend/src/components/modules/Landing/Landing.tsx
+++ frontend/src/components/modules/Landing/Landing.tsx
@@ -1,11 +1,42 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { Link as RouterLink } from "react-router-dom";
 import "./landing.css";
 
 const Landing: React.FC = () => {
   return (
-    <div>
-      <p> Hello World</p>
-    </div>
+    <Container maxWidth="lg" className="landing-container">
+      <Typography variant="h2" component="h1" gutterBottom>
+        Kendo Tournament Management
+      </Typography>
+      <Typography variant="h5" component="h3" gutterBottom>
+        Manage your tournaments with ease.
+      </Typography>
+      <div className="landing-buttons">
+        <Button
+          variant="contained"
+          color="primary"
+          component={RouterLink}
+          size="large"
+          to="/login"
+          className="button-large"
+        >
+          Login
+        </Button>
+        <Button
+          variant="outlined"
+          color="primary"
+          component={RouterLink}
+          to="/register"
+          size="large"
+          className="button-large outlined"
+        >
+          Register
+        </Button>
+      </div>
+    </Container>
   );
 };
 
