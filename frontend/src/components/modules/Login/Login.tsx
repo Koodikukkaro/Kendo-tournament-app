b20da2b4d1819e714f929585f96db3d79886fe36
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-11-09 01:53:37 +0200

    cors added on backend, frontend login UI updated with error component, api call to backend added

--- frontend/src/components/modules/Login/Login.tsx
+++ frontend/src/components/modules/Login/Login.tsx
@@ -8,6 +8,8 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Footer from "components/common/Footer/Footer";
 import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import CloseIcon from "@mui/icons-material/Close";
 
 interface LocationProps {
   state: {
@@ -20,20 +22,76 @@ interface FormData {
   password: string;
 }
 
+interface ErrorMessage {
+  message: string;
+}
+
 const LoginForm: React.FC = () => {
   const navigate = useNavigate();
   const { login } = useAuth();
   const location = useLocation() as unknown as LocationProps;
   const from = location.state?.from?.pathname ?? "/";
+  const loginAPI = "http://localhost:8080/api/auth/logis"; // transfer the base URL into env file.
 
   const [formData, setFormData] = useState<FormData>({
     login: "",
     password: ""
   });
 
-  const onHandleSubmit = async (): Promise<void> => {
-    await login();
-    navigate(from, { replace: true });
+  const [errorMessage, setErrorMessage] = useState<ErrorMessage>({
+    message: ""
+  });
+
+  const onHandleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault(); // Prevent the default form submit behavior
+    try {
+      const errorContext = ["requestBody.password", "requestBody.login"];
+      const response = await fetch(loginAPI, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: formData.login,
+          password: formData.password
+        })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        // Store the token, and update auth state
+        await login();
+        navigate(from, { replace: true });
+      } else {
+        // TODO: display a message to the user
+        const errorData = await response.json();
+        const context = errorData.errors[0].context;
+        for (let i = 0; i < errorContext.length; i++) {
+          const key = errorContext[i];
+          if (context[key] !== undefined) {
+            setErrorMessage({
+              message: context[key].message
+            });
+            break; // found the error message
+          }
+        }
+      }
+    } catch (error) {
+      // TODO: display a message to the user
+      console.log(error);
+      setErrorMessage({
+        message: "Unknown Error Occurred. Please try again later!"
+      });
+    }
+  };
+
+  const handleClose = (): void => {
+    setErrorMessage({
+      message: ""
+    });
   };
 
   const handleInputChange = (
@@ -59,6 +117,12 @@ const LoginForm: React.FC = () => {
                 Sign In!
               </Typography>
 
+              {errorMessage.message !== "" && (
+                <Paper className="error-message">
+                  <p>{errorMessage.message}</p>
+                  <CloseIcon className="cross-icon" onClick={handleClose} />
+                </Paper>
+              )}
               <TextField
                 label="Username/Email"
                 type="text"
@@ -112,9 +176,11 @@ const LoginForm: React.FC = () => {
             <Typography component="h3" variant="h3">
               Hello, Friend!
             </Typography>
-            <Typography variant="body1">This is a paragraph text.</Typography>
+            <Typography variant="body1">
+              Enter your personal details and start your journey with us!
+            </Typography>
             <br />
-            <Button variant="outlined" color="primary">
+            <Button variant="contained" color="success" className="sign-up-btn">
               <Link to="/register" className="sign-up-link">
                 Sign Up
               </Link>
