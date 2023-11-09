e59895fc84f9cc340898d4953b88801554962cef
melina <meruusunen@MacBook-Air-3.local>
2023-11-10 01:01:28 +0200

    add interface for formdata, fix tournament type typo and date handling

--- frontend/src/components/modules/CreateTournament/CreateTournament.tsx
+++ frontend/src/components/modules/CreateTournament/CreateTournament.tsx
@@ -3,15 +3,24 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./createtournament.css";
 
+export interface FormData {
+  tournamentName: string;
+  location: string;
+  startDate: Date;
+  endDate: Date;
+  description: string;
+  tournamentType: string;
+  organizer: boolean;
+  organizerEmail: string;
+  organizerTel: string;
+}
+
 const TournamentForm: React.FC = () => {
-  const today = new Date();
-
-  const [startDate, setStartDate] = useState<Date | null>(new Date());
-  const [endDate, setEndDate] = useState<Date | null>(new Date());
-
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     tournamentName: "",
     location: "",
+    startDate: new Date(),
+    endDate: new Date(),
     description: "",
     tournamentType: "",
     organizer: false,
@@ -41,6 +50,30 @@ const TournamentForm: React.FC = () => {
     setFormData((prevData) => ({
       ...prevData,
       [fieldName]: value
+    }));
+  };
+
+  const handleStartDateChange = (date: Date | null): void => {
+    setFormData((prevData) => ({
+      ...prevData,
+      startDate: date ?? new Date(),
+      endDate:
+        date !== null && prevData.endDate !== null && date > prevData.endDate
+          ? date
+          : prevData.endDate
+    }));
+  };
+
+  const handleEndDateChange = (date: Date | null): void => {
+    setFormData((prevData) => ({
+      ...prevData,
+      endDate: date ?? new Date(),
+      startDate:
+        date !== null &&
+        prevData.startDate !== null &&
+        date < prevData.startDate
+          ? date
+          : prevData.startDate
     }));
   };
 
@@ -90,26 +123,21 @@ const TournamentForm: React.FC = () => {
           <label htmlFor="startDate"></label>
           <DatePicker
             selectsStart
-            selected={startDate}
-            showIcon
-            minDate={today}
-            startDate={startDate}
-            endDate={endDate}
-            onChange={(date: Date | null) => {
-              setStartDate(date);
-            }}
+            selected={formData.startDate}
+            dateFormat="dd/MM/yyyy"
+            startDate={formData.startDate}
+            endDate={formData.endDate}
+            onChange={handleStartDateChange}
           />
           <label htmlFor="endDate"></label>
           <DatePicker
             selectsEnd
-            selected={endDate}
-            showIcon
-            startDate={startDate}
-            endDate={endDate}
-            minDate={startDate}
-            onChange={(date: Date | null) => {
-              setEndDate(date);
-            }}
+            selected={formData.endDate}
+            dateFormat="dd/MM/yyyy"
+            startDate={formData.startDate}
+            endDate={formData.endDate}
+            minDate={formData.startDate}
+            onChange={handleEndDateChange}
           />
         </div>
       </div>
@@ -135,13 +163,13 @@ const TournamentForm: React.FC = () => {
         <select
           id="tournamentType"
           onChange={(e) => {
-            handleSelectChange(e, "organizer");
+            handleSelectChange(e, "tournamentType");
           }}
           value={formData.tournamentType}
         >
           <option value="robin">Round Robin</option>
           <option value="playoff">Playoff</option>
-          <option value="premPlayoff">Premilinary + playoff</option>
+          <option value="preAndPlayoff">Premilinary + playoff</option>
         </select>
       </div>
       <br />
