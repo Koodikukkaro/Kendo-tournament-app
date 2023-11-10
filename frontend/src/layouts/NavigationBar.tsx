33c4a3afb302aa4cd3e64585002b2320aa8ef4e8
Samuel Helén <samuel.helen@protonmail.com>
2023-11-10 18:25:14 +0200

    Start navbar development by copying from mui

--- frontend/src/layouts/NavigationBar.tsx
+++ frontend/src/layouts/NavigationBar.tsx
@@ -1,59 +1,119 @@
+// TODO: Add desired links
 import React, { type ReactElement } from "react";
 import { NavLink, Outlet } from "react-router-dom";
-// import { Link, Outlet } from "react-router-dom";
-
-// // TODO: Implement proper layouts.
 
 // import Container from "@mui/material/Container";
-// import Box from "@mui/material/Box";
-// import Link from "@mui/material/Link";
 // import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
 // import Container from "@mui/material/Container";
 // import Link from "@mui/material/Link";
+import MenuIcon from "@mui/icons-material/Menu";
+import IconButton from "@mui/material/IconButton";
+import Drawer from "@mui/material/Drawer";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 
-const NavigationBar = (/* { user: } */): ReactElement => {
+interface Props {
+  window?: () => Window;
+}
+
+const navItems = ["navigaatioitemi1", "navigaatioitemi2"];
+const NavigationBar = (props: Props /* { user: } */): ReactElement => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = (): void => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      MUI
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit" component={NavLink} to="">
-            home
-          </Button>
-          <Button color="inherit">notes</Button>
-          <Button color="inherit">users</Button>
-          {/* user ? (
-            <em>{user} logged in</em>
-          ) : (
-            <Button color="inherit">login</Button>
-          ) */}
-        </Toolbar>
-      </AppBar>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="static" component="nav">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navItems.map((item) => (
+                <Button
+                  key={item}
+                  sx={{ color: "#fff" }}
+                  component={NavLink}
+                  to=""
+                >
+                  {item}
+                </Button>
+              ))}
+            </Box>
+            {/* <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              <Button color="inherit" component={NavLink} to="">
+                home
+              </Button>
+              <Button color="inherit">notes</Button>
+              <Button color="inherit">users</Button>
+              {// user ? (
+                <em>{user} logged in</em>
+              ) : (
+                <Button color="inherit">login</Button>
+              ) //}
+            </Box> */}
+          </Toolbar>
+        </AppBar>
+        <nav>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": { boxSizing: "border-box", width: 240 }
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </nav>
+      </Box>
       <Outlet />
     </>
   );
 };
-
-// import { NavLink, Outlet } from "react-router-dom";
-// // TODO: Add desired NavLinks
-// const NavigationBar = (): ReactElement => {
-//   return (
-//     <>
-//       <nav>
-//         <ul>
-//           <li>
-//             <NavLink to="">NavMessage</NavLink>
-//           </li>
-//           <li>
-//             <NavLink to="">Navigointia</NavLink>
-//           </li>
-//         </ul>
-//       </nav>
-//       <Outlet />
-//     </>
-//   );
-// };
 
 export default NavigationBar;
