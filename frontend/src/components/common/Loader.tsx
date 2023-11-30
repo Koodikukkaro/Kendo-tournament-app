4498bab6b9372ed324739de81962d268f6ca3d31
vilbedawg <vilho.luoma@gmail.com>
2023-11-30 16:56:06 +0200

    Add generic loader component

---
+++ frontend/src/components/common/Loader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import CircularProgress, {
+  CircularProgressProps
+} from "@mui/material/CircularProgress";
+
+const Loader = (props?: CircularProgressProps): React.ReactElement => {
+  return (
+    <Box textAlign="center">
+      <CircularProgress className="loader-icon" {...props} />
+    </Box>
+  );
+};
+
+export default Loader;
