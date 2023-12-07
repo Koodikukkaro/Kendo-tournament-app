dcb8c1b61768c7afd1ab5514856addf6039a0252
melina <meruusunen@MacBook-Air-3.wlan>
2023-12-07 21:29:29 +0200

    refactor useEffect and its contents, add past matches to sort, cleanup creatematchbuttons, fix updateplayerstats to work correctly, add sorting to scoretable generation

--- frontend/src/components/modules/Tournaments/OngoingTournament/RoundRobinTournamentView.tsx
+++ frontend/src/components/modules/Tournaments/OngoingTournament/RoundRobinTournamentView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, type ReactNode } from "react";
+import React, { useState, useEffect, type ReactNode, useRef } from "react";
 import {
   Tabs,
   Tab,
@@ -16,7 +16,6 @@ import "react-tabs/style/react-tabs.css";
 import { type User, type Match } from "types/models";
 import { useNavigate } from "react-router-dom";
 import { useTournament } from "context/TournamentContext";
-import api from "api/axios";
 
 interface tournamentPlayer {
   id: string;
@@ -28,172 +27,173 @@ interface tournamentPlayer {
 
 const RoundRobinTournamentView: React.FC = () => {
   const [selectedTab, setSelectedTab] = useState("scoreboard");
-  const [numberOfAttendees, setNumberOfAttendees] = useState(0);
-  const [tableData, setTableData] = useState<string[][]>([]);
   const [ongoingMatchElements, setOngoingMatchElements] = useState<ReactNode[]>(
     []
   );
   const [upcomingMatchElements, setUpcomingMatchElements] = useState<
     ReactNode[]
   >([]);
+  const [pastMatchElements, setPastMatchElements] = useState<ReactNode[]>([]);
 
   const tournament = useTournament();
   const [players, setPlayers] = useState<tournamentPlayer[]>([]);
   let ongoingMatches: Match[] = [];
   let upcomingMatches: Match[] = [];
+  let pastMatches: Match[] = [];
+
   const navigate = useNavigate();
+  const initialRender = useRef(true);
 
   useEffect(() => {
-    const getPlayerNames = (): void => {
-      setPlayers((prevPlayers) => {
-        const updatedPlayers = [...prevPlayers];
-
-        const playersObjects: User[] = tournament.players;
-        if (playersObjects.length > 0) {
-          for (const playerObject of playersObjects) {
-            const playerExists = updatedPlayers.some(
-              (player) => player.id === playerObject.id
-            );
-            if (!playerExists) {
-              updatedPlayers.push({
-                id: playerObject.id,
-                name: playerObject.firstName,
-                wins: 0,
-                losses: 0,
-                points: 0
-              });
-            }
-          }
-        }
-        console.log(updatedPlayers)
-
-        setNumberOfAttendees(updatedPlayers.length);
-        return updatedPlayers;
-      });
-    };
-
     getPlayerNames();
-
-    const sortMatches = (): void => {
-      const matches: Match[] = tournament.matchSchedule;
-      const currentDate = new Date();
-
-      if (matches.length > 0) {
-        const sortedMatches = matches
-          .filter((match) => match.timerStartedTimestamp !== undefined)
-          .sort((a, b) => {
-            if (
-              a.timerStartedTimestamp === null &&
-              b.timerStartedTimestamp === null
-            ) {
-              return 0;
-            } else if (a.timerStartedTimestamp === null) {
-              return -1;
-            } else if (b.timerStartedTimestamp === null) {
-              return 1;
-            } else {
-              const timeA = a.timerStartedTimestamp;
-              const timeB = b.timerStartedTimestamp;
-              return timeB.getTime() - timeA.getTime();
-            }
-          });
-
-        ongoingMatches = sortedMatches.filter(
-          (match) =>
-            match.timerStartedTimestamp !== null &&
-            match.timerStartedTimestamp <= currentDate
-        );
-        upcomingMatches = sortedMatches.filter(
-          (match) =>
-            match.timerStartedTimestamp === null ||
-            match.timerStartedTimestamp > currentDate
-        );
-      }
-    };
     sortMatches();
-    updatePlayerStats();
+    createMatchButtons();
   }, [tournament]);
 
   useEffect(() => {
-    const playerNames = players.map((player) => player.name);
-    const initialTableData: string[][] = playerNames.map((name) => [
-      name,
-      "0",
-      "0",
-      "0"
-    ]);
-    setTableData(initialTableData);
-  }, [tableData]);
-
-  useEffect(() => {
-    const createMatchButtons = async (): Promise<void> => {
-      const ongoingElements = [];
-      const upcomingElements = [];
-
-      const handleButtonClick = (matchId: string) => {
-        return () => {
-          navigate(`/matches/${matchId}`);
-        };
-      };
-
-      for (const [index, match] of ongoingMatches.entries()) {
-        const player1 = (await api.user.details(match.players[0].id)).firstName;
-        const player2 = (await api.user.details(match.players[1].id)).firstName;
-        ongoingElements.push(
-          <div style={{ marginBottom: "10px" }} key={index}>
-            <Button variant="contained" onClick={handleButtonClick(match.id)}>
-              {`${player1} - ${player2}`}
-            </Button>
-          </div>
-        );
-      }
-      for (const [index, match] of upcomingMatches.entries()) {
-        const player1 = (await api.user.details(match.players[0].id)).firstName;
-        const player2 = (await api.user.details(match.players[1].id)).firstName;
-        upcomingElements.push(
-          <div style={{ marginBottom: "10px" }} key={index}>
-            <Button variant="contained" onClick={handleButtonClick(match.id)}>
-              {`${player1} - ${player2}`}
-            </Button>
-          </div>
-        );
-      }
-      setOngoingMatchElements(ongoingElements);
-      setUpcomingMatchElements(upcomingElements);
-    };
-
-    void createMatchButtons();
+    if (initialRender.current) {
+      initialRender.current = false;
+      updatePlayerStats();
+    }
   }, []);
 
-  const updatePlayerStats = (): void => {
+  const getPlayerNames = (): void => {
     setPlayers((prevPlayers) => {
       const updatedPlayers = [...prevPlayers];
-  
-      for (const match of tournament.matchSchedule) {
-        if (match.winner) {
-          const winner = updatedPlayers.find((player) => player.id === match.winner);
-          const loser = updatedPlayers.find((playerId) => playerId.id !== match.winner);
-  
-          if (winner && loser) {
-            winner.wins += 1;
-            loser.losses += 1;
-          }
-  
-          for (const matchPlayer of match.players) {
-            const player = updatedPlayers.find((player) => player.id === matchPlayer.id);
-            if (player) {
-              player.points += matchPlayer.points.length;
-            }
+      const playersObjects: User[] = tournament.players;
+      if (playersObjects.length > 0) {
+        for (const playerObject of playersObjects) {
+          const playerExists = updatedPlayers.some(
+            (player) => player.id === playerObject.id
+          );
+          if (!playerExists) {
+            updatedPlayers.push({
+              id: playerObject.id,
+              name: playerObject.firstName,
+              wins: 0,
+              losses: 0,
+              points: 0
+            });
           }
         }
       }
       return updatedPlayers;
     });
   };
-  
 
-  const generateTableCells = (rowIndex: number): React.ReactNode[] => {
-    const player = players[rowIndex];
+  const sortMatches = (): void => {
+    const matches: Match[] = tournament.matchSchedule;
+    if (matches.length > 0) {
+      const sortedMatches = matches
+        .filter((match) => match.elapsedTime !== undefined)
+        .sort((a, b) => {
+          return b.elapsedTime - a.elapsedTime;
+        });
+      ongoingMatches = sortedMatches.filter(
+        (match) =>
+          match.elapsedTime !== null &&
+          match.elapsedTime > 0 &&
+          match.winner === undefined
+      );
+      upcomingMatches = sortedMatches.filter(
+        (match) =>
+          (match.elapsedTime === null || match.elapsedTime <= 0) &&
+          match.winner === undefined
+      );
+      pastMatches = sortedMatches.filter(
+        (match) =>
+          match.elapsedTime !== null &&
+          match.elapsedTime > 0 &&
+          match.winner !== undefined
+      );
+    }
+  };
+
+  const createMatchButtons = (): void => {
+    const createMatchButton = (
+      match: Match,
+      variant: "contained" | "contained" | "contained",
+      disabled?: boolean,
+      color?: "secondary"
+    ): JSX.Element => {
+      const player1 = players.find(
+        (player) => player.id === match.players[0].id
+      )?.name;
+      const player2 = players.find(
+        (player) => player.id === match.players[1].id
+      )?.name;
+
+      return (
+        <div style={{ marginBottom: "10px" }}>
+          <Button
+            variant={variant}
+            onClick={() => {
+              navigate(`/matches/${match.id}`);
+            }}
+            disabled={disabled}
+            color={color}
+          >
+            {`${player1} - ${player2}`}
+          </Button>
+        </div>
+      );
+    };
+
+    const ongoingElements = ongoingMatches.map((match, index) =>
+      createMatchButton(match, "contained")
+    );
+    const upcomingElements = upcomingMatches.map((match, index) =>
+      createMatchButton(match, "contained", true)
+    );
+    const pastElements = pastMatches.map((match, index) =>
+      createMatchButton(match, "contained", false, "secondary")
+    );
+
+    setOngoingMatchElements(ongoingElements);
+    setUpcomingMatchElements(upcomingElements);
+    setPastMatchElements(pastElements);
+  };
+
+  const updatePlayerStats = (): void => {
+    const processedMatches = new Set<string>();
+
+    setPlayers((prevPlayers) => {
+      const updatedPlayers = [...prevPlayers];
+
+      for (const match of tournament.matchSchedule) {
+        if (processedMatches.has(match.id)) {
+          continue;
+        }
+
+        if (match.winner !== undefined) {
+          const winner = updatedPlayers.find(
+            (player) => player.id === match.winner
+          );
+          const loser = updatedPlayers.find(
+            (player) => player.id !== match.winner
+          );
+
+          if (winner !== undefined && loser !== undefined) {
+            winner.wins += 1;
+            loser.losses += 1;
+          }
+
+          for (const matchPlayer of match.players) {
+            const player = updatedPlayers.find(
+              (player) => player.id === matchPlayer.id
+            );
+            if (player !== undefined) {
+              player.points += matchPlayer.points.length;
+            }
+          }
+          processedMatches.add(match.id);
+        }
+      }
+      return updatedPlayers;
+    });
+  };
+
+  const generateTableCells = (player: tournamentPlayer): ReactNode[] => {
     return [
       <TableCell key={0}>
         <Typography>{player.name}</Typography>
@@ -211,6 +211,8 @@ const RoundRobinTournamentView: React.FC = () => {
   };
 
   const generateTable = (): ReactNode => {
+    const sortedPlayers = [...players].sort((a, b) => b.points - a.points);
+
     const tableHeaders = ["Name", "Wins", "Losses", "Points"];
     return (
       <TableContainer component={Paper}>
@@ -223,8 +225,8 @@ const RoundRobinTournamentView: React.FC = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {Array.from({ length: numberOfAttendees }, (_, index) => (
-              <TableRow key={index}>{generateTableCells(index)}</TableRow>
+            {sortedPlayers.map((player, index) => (
+              <TableRow key={index}>{generateTableCells(player)}</TableRow>
             ))}
           </TableBody>
         </Table>
@@ -256,6 +258,10 @@ const RoundRobinTournamentView: React.FC = () => {
             <Typography variant="h5">Upcoming matches:</Typography>
           </div>
           <div>{upcomingMatchElements}</div>
+          <div>
+            <Typography variant="h5">Past matches:</Typography>
+          </div>
+          <div>{pastMatchElements}</div>
         </div>
       )}
     </>
