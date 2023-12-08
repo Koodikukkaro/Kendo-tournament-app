d61165a77841be4548032d8f2795b308fa4bae14
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-12-08 18:55:00 +0200

    Tournament Playoff multiple round view added

--- frontend/src/components/modules/Tournaments/OngoingTournament/PlayoffTournamentView.tsx
+++ frontend/src/components/modules/Tournaments/OngoingTournament/PlayoffTournamentView.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Bracket from "./TournamentBracket";
 import { type User, type Match } from "types/models";
 import { useTournament } from "context/TournamentContext";
-import { Typography, Box, Grid } from "@mui/material";
+import { Typography, Box, Grid, Divider } from "@mui/material";
 
 interface Rounds extends Record<number, Match[]> {}
 
@@ -17,50 +17,74 @@ const PlayoffTournamentView: React.FC = () => {
     if (acc[round] === undefined) {
       acc[round] = [];
     }
-
     acc[round].push(match);
     return acc;
   }, {});
+
+  const sortedRoundNumbers = Object.keys(rounds)
+    .map(Number)
+    .sort((a, b) => a - b);
 
   return (
     <Box
       sx={{
         overflowX: "auto",
-        whiteSpace: "nowrap",
         "&::-webkit-scrollbar": { display: "none" }
       }}
     >
-      <Grid container spacing={2}>
-        {Object.entries(rounds).map(([roundNumber, matches]) => (
-          <Grid item key={roundNumber} xs={12}>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "flex-start",
-                borderBottom: "1px solid black"
-              }}
-            >
-              <Typography
-                variant="h5"
-                style={{ marginBottom: 5, textAlign: "center" }}
-              >{`Round ${roundNumber}`}</Typography>
-              {matches.map((match: Match) => {
-                const tempPlayers: User[] = match.players.map((matchPlayer) => {
-                  const player = players.find((p) => p.id === matchPlayer.id);
-                  if (player === null || player === undefined) {
-                    throw new Error(
-                      `Player with ID ${matchPlayer.id} not found`
-                    );
-                  }
-                  return player;
-                });
-                return (
-                  <Bracket key={match.id} players={tempPlayers} match={match} />
-                );
-              })}
-            </Box>
-          </Grid>
+      <Grid
+        container
+        spacing={2}
+        justifyContent="flex-start"
+        alignItems="flex-start"
+      >
+        {sortedRoundNumbers.map((roundNumber, index) => (
+          <React.Fragment key={roundNumber}>
+            {index > 0 && (
+              <Divider
+                orientation="vertical"
+                flexItem
+                sx={{ borderColor: "black" }}
+              />
+            )}
+            <Grid item>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  minWidth: 300 // Ensures the box has a minimum width
+                }}
+              >
+                <Typography
+                  variant="h5"
+                  sx={{ marginBottom: 2, textAlign: "center" }}
+                >{`Round ${roundNumber}`}</Typography>
+                {rounds[roundNumber].map((match: Match) => {
+                  const tempPlayers: User[] = match.players.map(
+                    (matchPlayer) => {
+                      const player = players.find(
+                        (p) => p.id === matchPlayer.id
+                      );
+                      if (player === null || player === undefined) {
+                        throw new Error(
+                          `Player with ID ${matchPlayer.id} not found`
+                        );
+                      }
+                      return player;
+                    }
+                  );
+                  return (
+                    <Bracket
+                      key={match.id}
+                      players={tempPlayers}
+                      match={match}
+                    />
+                  );
+                })}
+              </Box>
+            </Grid>
+          </React.Fragment>
         ))}
       </Grid>
     </Box>
