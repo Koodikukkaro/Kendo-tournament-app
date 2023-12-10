b16e04354b32d26a0fb3bf254b2ab37a0131e470
vilbedawg <vilho.luoma@gmail.com>
2023-12-10 17:26:16 +0200

    Style fixes for signup and upcoming tournament view tables

--- frontend/src/components/modules/Tournaments/UpcomingTournamentView.tsx
+++ frontend/src/components/modules/Tournaments/UpcomingTournamentView.tsx
@@ -5,10 +5,59 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import { useAuth } from "context/AuthContext";
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import Paper from "@mui/material/Paper";
+import type { Tournament } from "types/models";
+
+const generateTable = (tournament: Tournament): React.ReactNode => {
+  const tableHeaders = ["Name", "Rank", "Club"] as const;
+  return (
+    <TableContainer component={Paper} aria-label="Player Table">
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            {tableHeaders.map((header) => (
+              <TableCell key={header} aria-label={`header-${header}`}>
+                {header}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tournament.players.map((player, index) => (
+            <TableRow key={player.id} aria-label={`player-${index}`}>
+              <TableCell aria-label={`cell-name-${index}`}>
+                <Typography>
+                  {`${player.firstName} - ${player.lastName}`}
+                </Typography>
+              </TableCell>
+              <TableCell aria-label={`cell-rank-${index}`}>
+                <Typography>{player.danRank ?? "-"}</Typography>
+              </TableCell>
+              <TableCell aria-label={`cell-club-${index}`}>
+                <Typography>{player.clubName ?? "-"}</Typography>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 const UpcomingTournamentView: React.FC = () => {
   const navigate = useNavigate();
+  const { userId } = useAuth();
   const tournament = useTournament();
+  const userAlreadySigned = tournament.players.some(
+    (player) => player.id === userId
+  );
 
   return (
     <Container
@@ -54,35 +103,32 @@ const UpcomingTournamentView: React.FC = () => {
 
       <br />
 
-      <Box>
-        <Typography variant="body1" className="header">
-          Want to attend?
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            navigate("sign-up");
-          }}
-        >
-          Sign up
-        </Button>
-      </Box>
-      <br />
+      {!userAlreadySigned && (
+        <Box>
+          <Typography variant="body1" className="header">
+            Want to attend?
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={userAlreadySigned}
+            onClick={() => {
+              navigate("sign-up");
+            }}
+          >
+            Sign up
+          </Button>
+          <br />
+        </Box>
+      )}
 
       {tournament.players.length > 0 ? (
-        <>
+        <React.Fragment>
           <Typography variant="body1" className="header" fontWeight="bold">
             Others who have signed up:
           </Typography>
-
-          {tournament.players.map((player, index) => (
-            <Typography key={index} variant="body1" className="subtext">
-              Name: {player.firstName} {player.lastName}, Rank: {player.danRank}
-              , Club: {player.clubName}
-            </Typography>
-          ))}
-        </>
+          {generateTable(tournament)}
+        </React.Fragment>
       ) : (
         <Typography variant="body1" className="header" fontWeight="bold">
           No players have signed up yet.
