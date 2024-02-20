2dca9a6fce279291b8371f572c0e7478d620560b
tktstudent <mikaelkonu10@gmail.com>
2024-02-20 22:37:09 +0200

    Add playoff result functionality

--- frontend/src/components/modules/Tournaments/OngoingTournament/TournamentBracket.tsx
+++ frontend/src/components/modules/Tournaments/OngoingTournament/TournamentBracket.tsx
@@ -31,6 +31,32 @@ const Bracket: React.FC<BracketProps> = ({ match, players }) => {
   const player1Name = `${player1.firstName} ${player1.lastName}`;
   const player2Name = `${player2.firstName} ${player2.lastName}`;
 
+  // Initialize variables for player points
+  let player1Points = 0;
+  let player2Points = 0;
+
+  // Increments the total points of the player by 1 for each scored point
+  // If the point type is "hansoku", the opponent receives 0.5 points
+  for (const point of match.players[0].points) {
+    if (point.type === "hansoku") {
+      player2Points += 0.5;
+    }
+    else {
+      player1Points++;
+    }
+  }
+  for (const point of match.players[1].points) {
+    if (point.type === "hansoku") {
+      player1Points += 0.5;
+    }
+    else {
+      player2Points ++;
+    }
+  }
+  // Round down each player's total points to the nearest integer
+  player1Points = Math.floor(player1Points);
+  player2Points = Math.floor(player2Points);
+
   let player1Color = "black";
   let player2Color = "black";
 
@@ -62,6 +88,13 @@ const Bracket: React.FC<BracketProps> = ({ match, players }) => {
             <Typography textAlign="center" style={{ color: player2Color }}>
               {player2Name}
             </Typography>
+            {isWinnerDeclared && (
+              <Typography textAlign="center" variant="h6">
+                <span style={{ color: player1Color }}>{player1Points}</span>
+                  - 
+                <span style={{ color: player2Color }}>{player2Points}</span>
+              </Typography>
+            )}
           </CardContent>
         </CardActionArea>
       </Card>
